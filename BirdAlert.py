5be3932101c5df89eacf7d9f82408cf9983dafb5
pRs3k <160817153+pRs3k@users.noreply.github.com>
2024-09-07 13:59:15 -0600

    v0.4.0
    -Added hex watch list
    -Added military hex ranges
    -Terminal message cleanup
    -Added more commercial airlines

--- BirdAlert.py
+++ BirdAlert.py
@@ -12,8 +12,8 @@ from math import radians, cos, sin, asin, sqrt, atan2, degrees
 update_rate = 5                                    # The frequency that this script runs checking for aircraft updates, in seconds
 aircraft_json_path = "/run/readsb/aircraft.json"   # Change this if your aircraft.json is in a different location
 range_miles = 20                                   # Change this to the radius of the notification zone (in miles)
-min_alert_period = 1200                            # Change this to the number of seconds to wait before alerting again for the same aircraft
-include_military_check = True                      # Change to False if you don't want to alert based on the military flag being set (True will alert regardless of other settings)
+min_alert_period = 600                             # Change this to the number of seconds to wait before alerting again for the same aircraft
+include_military_check = True                      # Change to False if you don't want to alert based on the military flag being set or the hex falling within the military range  (True will alert regardless of other settings)
 include_emergency_check = True                     # Change to False if you don't want to alert based on the emergency flag being set (True will alert regardless of other settings)
 skip_commercial = True                             # Change to False to include alerts for commerical aircraft (doesn't effect notificaitons for emergency flag being set)
 transponder_types = [                              # Comment in/out rows corresponding to transponder types you want to receive alerts for
@@ -27,6 +27,16 @@ transponder_types = [                              # Comment in/out rows corresp
     'adsb_other',                                  # ADS-B transponder using a non-ICAO address, such as an anonymized address
     'adsr_other',                                  # Rebroadcast of ADS-B messages originally sent via UAT transponder, using a non-ICAO address
     'tisb_other',                                  # Non-ADS-B target using a non-ICAO address
+]
+
+watch_list = [                                     # Add the hex codes of specific aircraft to always alert on
+    'a2575b', #B-25J Mitchell "Yellow Rose"
+    'a082d8' #C-130 firefighting
+    'a69072' #DC-10 firefighting   
+    'a1a640' #UH-1 firefighting
+    'a4bf3d' #CH-47 firefighting
+    'a1a640' #UH-1 firefighting
+    'a270c6' #DC-3 firefighting
 ]
 
 # Your location
@@ -63,6 +73,54 @@ signal_recipients = []  # change this to a list of recipients
 # Tracking last notification times
 last_notified = {}
 
+
+def is_military_aircraft(hex_code):
+    # Convert the hex code to an integer
+    hex_int = int(hex_code, 16)
+
+    military_ranges = [
+        ("adf7c8", "afffff"),
+        ("010070", "01008f"),
+        ("0a4000", "0a4fff"),
+        ("33ff00", "33ffff"),
+        ("350000", "37ffff"),
+        ("3aa000", "3affff"),
+        ("3b7000", "3bffff"),
+        ("3ea000", "3ebfff"),
+        ("3f4000", "3fbfff"),
+        ("400000", "40003f"),
+        ("43c000", "43cfff"),
+        ("444000", "446fff"),
+        ("44f000", "44ffff"),
+        ("457000", "457fff"),
+        ("45f400", "45f4ff"),
+        ("468000", "4683ff"),
+        ("473c00", "473c0f"),
+        ("478100", "4781ff"),
+        ("480000", "480fff"),
+        ("48d800", "48d87f"),
+        ("497c00", "497cff"),
+        ("498420", "49842f"),
+        ("4b7000", "4b7fff"),
+        ("4b8200", "4b82ff"),
+        ("70c070", "70c07f"),
+        ("710258", "71028f"),
+        ("710380", "71039f"),
+        ("738a00", "738aff"),
+        ("7cf800", "7cfaff"),
+        ("800200", "8002ff"),
+        ("c20000", "c3ffff"),
+        ("e40000", "e41fff")
+    ]
+
+    # Check if the hex code is within any of the military ranges
+    for start, end in military_ranges:
+        start_int = int(start, 16)
+        end_int = int(end, 16)
+        if start_int <= hex_int <= end_int:
+            return True
+    return False
+
 # Function to calculate the distance between two lat/lon pairs using the Haversine formula
 def haversine(lat1, lon1, lat2, lon2):
     lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
@@ -89,7 +147,7 @@ def calculate_direction(lat1, lon1, lat2, lon2):
 # Function to send an email notification
 def send_email_notification(aircraft_info, hex_code, distance, direction):
     subject = "Bird Alert!"
-    military_status = "Yes" if aircraft_info.get('military') else "Unknown"
+    military_status = "Yes" if aircraft_info.get('military', False) or is_military_aircraft(hex_code) else "Unknown"
     ground_speed = aircraft_info.get('gs', 'N/A')
     emergency_status = aircraft_info.get('emergency', 'none')
 
@@ -99,16 +157,15 @@ def send_email_notification(aircraft_info, hex_code, distance, direction):
               f"Military: {military_status}\n Ground Speed: {ground_speed} knots\n" \
               f"Emergency: {emergency_status}\n"
 
-    print(f"Sending Email: {message}")
-
     try:
         with smtplib.SMTP(your_smtp_server, your_smtp_port) as server:
             server.starttls()
             server.login(your_email, your_email_app_password)
             server.sendmail(your_email, your_email, message)
+            print(f"Sending Email: {message}\n")
         return True
     except Exception as e:
-        print(f"Failed to send email: {e}")
+        print(f"Failed to send email: {e}\n")
         return False
 
 # Function to send a Telegram notification
@@ -125,10 +182,10 @@ def send_telegram_notification(message):
     try:
         response = requests.post(url, json=payload)
         response.raise_for_status()
-        print("Telegram message sent")
+        print("Telegram message sent\n")
         return True
     except Exception as e:
-        print(f"Failed to send Telegram message: {e}")
+        print(f"Failed to send Telegram message: {e}\n")
         return False
 
 # Function to send a Pushover notification
@@ -146,10 +203,10 @@ def send_pushover_notification(message):
     try:
         response = requests.post(url, data=payload)
         response.raise_for_status()
-        print("Pushover message sent")
+        print("Pushover message sent\n")
         return True
     except Exception as e:
-        print(f"Failed to send Pushover message: {e}")
+        print(f"Failed to send Pushover message: {e}\n")
         return False
 
 # Function to send a notification via IFTTT
@@ -163,10 +220,10 @@ def send_ifttt_notification(message):
     try:
         response = requests.post(url, json=payload)
         response.raise_for_status()
-        print("IFTTT notification sent")
+        print("IFTTT notification sent\n")
         return True
     except Exception as e:
-        print(f"Failed to send IFTTT notification: {e}")
+        print(f"Failed to send IFTTT notification: {e}\n")
         return False
 
 # Function to send a Signal notification
@@ -177,10 +234,10 @@ def send_signal_notification(message):
     try:
         for recipient in signal_recipients:
             # Replace with actual Signal command or API call to send a message
-            print(f"Sending Signal message to {recipient}: {message}")
+            print(f"Sending Signal message to {recipient}: {message}\n")
         return True
     except Exception as e:
-        print(f"Failed to send Signal message: {e}")
+        print(f"Failed to send Signal message: {e}\n")
         return False
 
 # Function to send notifications through all available methods
@@ -188,7 +245,7 @@ def send_notification(aircraft_info, hex_code, distance, direction):
     message = f"Bird Alert!\n" \
               f"Aircraft hex: {hex_code}\n Distance: {distance:.2f}mi\n Direction: {direction}\n" \
               f"Transponder: {aircraft_info.get('type', 'N/A')}\n Callsign: {aircraft_info.get('flight', 'N/A')}\n" \
-              f"Military: {'Yes' if aircraft_info.get('military') else 'Unknown'}\n" \
+              f"Military: {'Yes' if aircraft_info.get('military', False) or is_military_aircraft(hex_code) else 'Unknown'}\n" \
               f"Ground Speed: {aircraft_info.get('gs', 'N/A')} knots\n" \
               f"Emergency: {aircraft_info.get('emergency', 'none')}\n"
 
@@ -210,12 +267,13 @@ def check_aircraft(aircraft):
     lat = aircraft.get('lat')
     lon = aircraft.get('lon')
     transponder_type = aircraft.get('type', 'N/A')
-    military_flag = "Yes" if aircraft.get('military') else "Unknown"
+    military_flag = "Yes" if aircraft.get('military', False) or is_military_aircraft(hex_code) else "Unknown"
     emergency_flag = aircraft.get('emergency', 'none')
     flight = aircraft.get('flight', 'N/A')
     global skip_commercial
     global transponder_types
     global min_alert_period
+    global watch_list
 
     # Calculate distance and direction only if lat and lon are available
     if lat is not None and lon is not None:
@@ -232,7 +290,16 @@ def check_aircraft(aircraft):
             print(f"Skipping aircraft hex {hex_code}: Distance={dist:.2f}mi is outside of notification range.\n\n")
             return
 
-        # 2. Check if the emergency flag is set
+        # 2. Check if the hex code matches the watch list
+        if hex_code in watch_list:
+            if hex_code not in last_notified or (current_time - last_notified[hex_code]) > min_alert_period:
+                send_notification(aircraft, hex_code, dist, direction)
+                last_notified[hex_code] = current_time
+            else:
+                print(f"Skipping aircraft hex {hex_code}: Last alert sent {int((current_time - last_notified[hex_code]) / 60)} minutes ago.\n\n")
+            return
+
+        # 3. Check if the emergency flag is set
         if (include_emergency_check and emergency_flag != 'none'):
             if hex_code not in last_notified or (current_time - last_notified[hex_code]) > min_alert_period:
                 send_notification(aircraft, hex_code, dist, direction)
@@ -241,16 +308,16 @@ def check_aircraft(aircraft):
                 print(f"Skipping aircraft hex {hex_code}: Last alert sent {int((current_time - last_notified[hex_code]) / 60)} minutes ago.\n\n")
             return
 
-        # 3. Check if the callsign belongs to a commercial airline
+        # 4. Check if the callsign belongs to a commercial airline
         if skip_commercial == False:
             pass
         else:
-            if flight.startswith(('SWA', 'UAL', 'DAL', 'SKW', 'ASA', 'ATN', 'EJA', 'AAL', 'AFR', 'AIC', 'ANA', 'BAW', 'CPA', 'DLH', 'EIN', 'ETD', 'EZY', 'JAL', 'KLM', 'QFA', 'RYR', 'SIA', 'THA', 'UAE', 'VIR', 'FDX')):
+            if flight.startswith(('SWA', 'UAL', 'DAL', 'SKW', 'ASA', 'ATN', 'EJA', 'AAL', 'AFR', 'AIC', 'ANA', 'BAW', 'CPA', 'DLH', 'EIN', 'ETD', 'EZY', 'JAL', 'KLM', 'QFA', 'RYR', 'SIA', 'THA', 'UAE', 'VIR', 'FDX', 'FFT', 'CXK', 'AAY', 'LYM', 'CRE', 'JTL', 'ACA', 'EJM', 'UPS', 'MVJ')):
                 print(f"Skipping aircraft hex {hex_code}: Commercial airline.\n\n")
                 return
 
-        # 4. Check if the military flag is set
-        if (include_military_check and aircraft.get('military', False)):
+        # 5. Check if the hex code or flag indicates military
+        if (include_military_check and (aircraft.get('military', False) or is_military_aircraft(hex_code))):
             if hex_code not in last_notified or (current_time - last_notified[hex_code]) > min_alert_period:
                 send_notification(aircraft, hex_code, dist, direction)
                 last_notified[hex_code] = current_time
@@ -258,7 +325,7 @@ def check_aircraft(aircraft):
                 print(f"Skipping aircraft hex {hex_code}: Last alert sent {int((current_time - last_notified[hex_code]) / 60)} minutes ago.\n\n")
             return
         
-        # 5. Check the transponder type
+        # 6. Check the transponder type
         if transponder_type in transponder_types:
             if hex_code not in last_notified or (current_time - last_notified[hex_code]) > min_alert_period:
                 send_notification(aircraft, hex_code, dist, direction)
